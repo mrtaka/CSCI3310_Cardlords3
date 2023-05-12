22fd5f620ad1552179605fa11dec00108fc453eb
fymf114 <yeungmingfaifrankie@gmail.com>
2023-05-13 02:38:26 +0800

    update login as guest, implement check userType, assign userType and deck for new accounts

--- app/src/main/java/com/example/cardlords3/MenuActivity.java
+++ app/src/main/java/com/example/cardlords3/MenuActivity.java
@@ -2,19 +2,63 @@ package com.example.cardlords3;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cardlords3.ui.login.GameActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private static final String TAG = "MenuActivity";
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        //test.
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        if (user != null) {
+            db.collection("users").document(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            int userType = Math.toIntExact(document.getLong("userType"));
+                            Log.d(TAG, "User type: " + userType);
+                            // Continue processing the user type...
+                        } else {
+                            Log.d(TAG, "No such document");
+                            //New account
+                            //Default: set userType as normal user and set default inventory.
+                            Map<String, Object> userDoc = new HashMap<>();
+                            userDoc.put("userType", 1); // 0 - admin, 1 - normal user, 2 - guest
+                            userDoc.put("inventory", Arrays.asList(1, 2, 3)); // list of card IDs
+                            db.collection("users").document(user.getUid()).set(userDoc);
+                        }
+                    } else {
+                        Log.d(TAG, "get failed with ", task.getException());
+                    }
+                }
+            });
+        }
     }
 
     //when click pve mode
